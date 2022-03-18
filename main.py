e876bb542d7e3aa92d057f47a3f52802a5e3b338
yu2799 <ura.yu.2799@gmail.com>
2022-03-18 16:19:46 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from requests import post
+from json import dumps
+from os import environ
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()
+    URL = environ["END_POINT"]
+    main_content = {'content': '送るテキスト'}
+    headers = {'Content-Type': 'application/json'}
+
+    response = post(
+        URL, dumps(main_content), headers=headers)
+
+
+if __name__ == '__main__':
+    main()
